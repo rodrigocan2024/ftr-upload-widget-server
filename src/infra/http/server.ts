1a104410a15d1421e747b853d1684cdf58d859b5
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-05 17:24:24 -0300

    05: Documentação com Swagger

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,14 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
-
-import { env } from '@/env'
 import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
@@ -33,9 +35,22 @@ server.register(fastifyCors, {
   origin: '*',
 })
 
-server.register(uploadImageRoute)
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
 
-console.log(env.DATABASE_URL)
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
