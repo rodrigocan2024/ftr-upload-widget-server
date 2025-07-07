3745323d2b4619ef5089e787cea0ae42379d05d4
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-07-07 15:37:43 -0300

    20: Função: exportar uploads (CSV)

--- src/app/functions/export-uploads.ts
+++ src/app/functions/export-uploads.ts
@@ -1,7 +1,11 @@
+import { PassThrough, Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import { stringify } from 'csv-stringify'
 import { ilike } from 'drizzle-orm'
 import { z } from 'zod'
 import { db, pg } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
 import { type Either, makeRight } from '@/shared/either'
 
 const exportUploadsInput = z.object({
@@ -34,9 +38,49 @@ export async function exportUploads(
 
   const cursor = pg.unsafe(sql, params as string[]).cursor(2)
 
-  for await (const rows of cursor) {
-    console.log(rows)
-  }
+  // const csv
 
-  return makeRight({ reportUrl: '' })
+  // for await (const rows of cursor) {
+  //   console.log(rows)
+  // }
+
+  const csv = stringify({
+    delimiter: ',',
+    header: true,
+    columns: [
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' },
+      { key: 'remote_url', header: 'Remote URL' },
+      { key: 'created_at', header: 'Uploaded at' },
+    ],
+  })
+
+  const uploadToStorageStream = new PassThrough()
+
+  const convertToCsvPipeline = pipeline(
+    cursor,
+    new Transform({
+      objectMode: true,
+      transform(chunks: unknown[], encoding, callback) {
+        for (const chunk of chunks) {
+          this.push(chunk)
+        }
+
+        callback()
+      },
+    }),
+    csv,
+    uploadToStorageStream
+  )
+
+  const uploadToStorage = uploadFileToStorage({
+    contentType: 'text/csv',
+    folder: 'downloads',
+    fileName: `${new Date().toISOString()}-uploads.csv`,
+    contentStream: uploadToStorageStream,
+  })
+
+  const [{ url }] = await Promise.all([uploadToStorage, convertToCsvPipeline])
+
+  return makeRight({ reportUrl: url })
 }
